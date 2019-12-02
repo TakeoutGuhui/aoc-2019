f4db6b8fcb9ee9f27a392bf89908ff925e3a82bf
Hampus Gunnarsson <takeoutguhui@gmail.com>
2019-12-02 22:07:04 +0100

    Some refactoring

--- day_2/part_1_2.py
+++ day_2/part_1_2.py
@@ -1,13 +1,10 @@
 import sys
 
-def parse_opcode(memory, pc):
-    return (memory[pc], memory[pc+1], memory[pc+2], memory[pc+3])
-
 
 def execute_instruction(memory, pc):
     if memory[pc] == 99:
         return False
-    opcode, input_one, input_two, output = parse_opcode(memory, pc) 
+    opcode, input_one, input_two, output = memory[pc:pc+4]
     if opcode == 1:
         memory[output] = memory[input_one] + memory[input_two]
         return True
@@ -16,7 +13,7 @@ def execute_instruction(memory, pc):
         return True
 
 
-def run_program(memory, pc):
+def run_program(memory, pc=0):
     while execute_instruction(memory, pc):
         pc += 4
 
@@ -26,24 +23,19 @@ with open('input.txt') as f:
 
 # Part 1
 memory = program.copy()
-memory[1] = 12
-memory[2] = 2
-run_program(memory, 0)
+memory[1], memory[2] = 12, 2
+run_program(memory)
 print(f"Answer to part 1: {memory[0]}")
 
 # Part 2
 for x in range(100):
     for y in range(100):
         memory = program.copy()
-        memory[1] = x
-        memory[2] = y
+        memory[1], memory[2] = x, y
 
-        run_program(memory, 0)
+        run_program(memory)
 
         if memory[0] == 19690720:
-            result = 100 * memory[1] + memory[2]
+            result = 100 * x + y
             print(f"Answer to part 2: {result}")
             sys.exit()
-        
-
-
